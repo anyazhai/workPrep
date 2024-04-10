129445705c2ac261fdb38158b33dd3678245d866
Chaithanya S <chaithanyas.work@gmail.com>
2024-04-10 11:03:41 +0530

    router parameters included

--- src/app/routingex/users/user/user.component.ts
+++ src/app/routingex/users/user/user.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+    //observables help to work with asynchronous tasks
+    //here it's used to observe any changes that happens to the route parameters from within the page itself 
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params ) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe(); //angular does this behind the scenes but it'll be better to mention it ourselves
+    
+    //if the subscription isn't destroyed when the component is destroyed then it'll keep on living in the memory and a new subscription is creation everytime the component is revisited(created)
   }
 
 }
