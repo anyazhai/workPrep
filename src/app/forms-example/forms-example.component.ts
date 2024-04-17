214791fbfc44b451e52bc64b1addb311397f8015
anyazhai <chaithanyachaithanya2708@gmail.com>
2024-04-17 09:48:38 +0530

    TD form validation

--- src/app/forms-example/forms-example.component.ts
+++ src/app/forms-example/forms-example.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-example',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './forms-example.component.css'
 })
 export class FormsExampleComponent {
+  @ViewChild('f') signupForm: NgForm;
 
+  suggestUserName(){
+    const suggestedName = 'Superuser'
+  }
+
+  // onSubmit(form: NgForm){
+  //   console.log(form)
+  // }
+
+  onSubmit(){
+    console.log(this.signupForm.value)
+    console.log(this.signupForm.valid)
+  }
 }
